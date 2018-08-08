f16ce7d6baec051ee1f187fc71b6f18bdbc5917c
Justin Ouellette <justin.ouellette@gmail.com>
2018-08-08 10:40:41 -0400

    removes warnings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,5 @@ class ApplicationController < ActionController::Base
     def allow_iframe
       response.headers.except! "X-Frame-Options"
       response.set_header "X-XSS-Protection", "0"
-      response.set_header "X-Warning", "HC SVNT DRACONES"
     end
 end
